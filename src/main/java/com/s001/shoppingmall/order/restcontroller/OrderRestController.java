9e88ca2ba2411b97507732907181aff127dc4c89
양유경 <gosjorb@gmail.com>
2024-03-19 16:06:42 +0900

    Feat: 결제 상태와 배송 상태 드롭다운으로 실시간 업데이트

--- src/main/java/com/s001/shoppingmall/order/restcontroller/OrderRestController.java
+++ src/main/java/com/s001/shoppingmall/order/restcontroller/OrderRestController.java
@@ -1,15 +1,13 @@
 package com.s001.shoppingmall.order.restcontroller;
 
 import com.s001.shoppingmall.order.dto.OrderRegisterParam;
+import com.s001.shoppingmall.order.dto.OrderUpdateParam;
 import com.s001.shoppingmall.order.service.OrderService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -32,5 +30,12 @@ public class OrderRestController {
                 .toUri();
 
         return ResponseEntity.created(location).body(orderId);
+    }
+
+    @PatchMapping("/{orderId}")
+    public ResponseEntity<?> updateOrderStatus(@PathVariable("orderId") Integer orderId, @RequestBody OrderUpdateParam param) {
+
+        orderService.update(orderId, param);
+        return ResponseEntity.ok().build();
     }
 }
