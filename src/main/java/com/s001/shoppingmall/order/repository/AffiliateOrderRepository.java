1fea559b768d1bfbb5f10323feb304b87a46a205
양유경 <gosjorb@gmail.com>
2024-03-21 14:55:20 +0900

    Feat: 주문 생성 및 상태 변경 시 제휴 주문 관련 구성
    - 주문 생성 : 제휴 서비스에 대한 정보가 들어오면 제휴 주문
      테이블에 추가합니다.
    - 배송 상태 변경
    
    TODO
    - 주문 생성 시 주문에 대한 정보 제휴 서비스 api 호출
    - 배송 상태 변경 시 해당 이벤트 내용에 대한 제휴 서비스 api 호출

---
+++ src/main/java/com/s001/shoppingmall/order/repository/AffiliateOrderRepository.java
@@ -0,0 +1,7 @@
+package com.s001.shoppingmall.order.repository;
+
+import com.s001.shoppingmall.order.entity.AffiliateOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AffiliateOrderRepository extends JpaRepository<AffiliateOrder, Integer> {
+}
