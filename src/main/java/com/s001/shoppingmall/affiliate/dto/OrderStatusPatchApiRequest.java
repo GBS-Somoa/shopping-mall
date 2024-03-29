61ff07af210ec89cd9c11bbf0772a4a1c603318f
gusckd96 <gusckd96@naver.com>
2024-03-29 12:49:59 +0900

    Feat: 소모아 배송 상태 변경 API 양식 변경

--- src/main/java/com/s001/shoppingmall/affiliate/dto/OrderStatusPatchApiRequest.java
+++ src/main/java/com/s001/shoppingmall/affiliate/dto/OrderStatusPatchApiRequest.java
@@ -7,17 +7,17 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class OrderStatusPatchApiRequest {
+public class
+OrderStatusPatchApiRequest {
 
 	private String orderStatus;
 
 	public static OrderStatusPatchApiRequest of(DeliveryStatus deliveryStatus) {
-		// 주문 완료, 배송중, 배송 완료, 주문 취소
 		String value = switch (deliveryStatus) {
 			case ORDER_CANCELED -> "주문 취소";
 			case ORDER_COMPLETED -> "주문 완료";
 			case DELIVERY_COMPLETED -> "배송 완료";
-			case DELIVERY_IN_PROGRESS -> "배송중";
+			case DELIVERY_IN_PROGRESS -> "배송 중";
 		};
 		return new OrderStatusPatchApiRequest(value);
 	}
